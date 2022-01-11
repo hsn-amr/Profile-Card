20bc8b6c847c52e0b9507e70187400e153d079ec
Hassan Al-Shammari <hsn999hsn@gmail.com>
2022-01-11 11:24:55 +0300

    Initial Commit

---
+++ Profile Card/Profile_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_CardApp.swift
+//  Profile Card
+//
+//  Created by TURKI ALFAISAL on 08/06/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct Profile_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
